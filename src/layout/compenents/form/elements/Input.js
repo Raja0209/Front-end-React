f59dbabd6558678af4831cca4bf331cd4e89df80
limostr <oussama.limam@gmail.com>
2019-11-23 11:17:35 +0100

    Form elements first commit

---
+++ src/layout/compenents/form/elements/Input.js
@@ -0,0 +1,13 @@
+import React,{ Componenet } from 'react';
+export default class Input extends Componenet{
+
+    render(){
+
+
+        return (
+            <div>
+            </div>
+        );
+    }
+
+}
